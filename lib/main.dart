4cf264a99bc8792e58927160059db4497e083961
Harsh Kandoi <harshkandoi8@gmail.com>
2023-03-15 12:36:34 +0530

    my quiz app

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,20 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Widget> scorekeeper = [
+    Icon(Icons.check_box, color: Colors.green),
+    Icon(
+      Icons.close_outlined,
+      color: Colors.red,
+    ),
+  ];
+  List<String> question = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  List<bool> answer = [false, true, true];
+  int questionnumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +51,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                question[questionnumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -50,41 +64,65 @@ class _QuizPageState extends State<QuizPage> {
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.green,
               ),
               onPressed: () {
-                //The user picked true.
+                bool correctanswer = answer[questionnumber];
+                if (correctanswer == true) {
+                  print('user is right');
+                } else {
+                  print(('User is wrong'));
+                }
+                setState(() {
+                  questionnumber++;
+                  scorekeeper.add(
+                    Icon(
+                      Icons.check_box,
+                      color: Colors.green,
+                    ),
+                  );
+                });
               },
+              child: Text(
+                'TRUE',
+              ),
             ),
           ),
         ),
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.red, // foreground
               ),
               onPressed: () {
-                //The user picked false.
+                bool correctanswer = answer[questionnumber];
+                if (correctanswer == false) {
+                  print('user is right');
+                } else {
+                  print('User is wrong');
+                }
+                setState(() {
+                  questionnumber++;
+                  scorekeeper.add(
+                    Icon(
+                      Icons.close_sharp,
+                      color: Colors.red,
+                    ),
+                  );
+                });
               },
+              child: Text('FALSE'),
             ),
           ),
+          //TODO: Add a Row here as your score keeper
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scorekeeper,
+        ),
       ],
     );
   }
